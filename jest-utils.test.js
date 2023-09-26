6538bd4c3d07a72c31cbccfd87c60e171c0c17b2
oka.ats@gmail.com <oka.ats@gmail.com>
2023-09-26 15:32:26 +0900

    remove jest

--- jest-utils.test.js
+++
@@ -1,8 +0,0 @@
-
-const { createContext } = require('asynchronous-context/context-factory');
-const testdb = require('asynchronous-context/jest-utils').createJestTester( test, createContext, {autoCommit:false, showReport:true,coloredReport:true, reportMethod:'stderr' } );
-
-testdb('two plus two is four', async function () {
-  expect(2 + 2).toBe(4);
-  return 'successful';
-});
