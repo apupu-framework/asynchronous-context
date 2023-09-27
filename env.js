b8c2310dc7ca7414696fc28320d9bf9d422d5be4
oka.ats@gmail.com <oka.ats@gmail.com>
2023-09-27 15:59:20 +0900

    throw an error if the "env" field is missing in .settings.json

--- env.js
+++ env.js
@@ -1,7 +1,13 @@
 
+const { readSettings, filenameOfSettings } = require( './settings' );
+
 const env = (settings)=>{
-  const inenv = settings?.env ?? {};
+  const inenv = settings?.env ?? null;
   const outenv = process.env;
+
+  if ( inenv === null ) {
+    throw new Error( `.env entry was not found in ${filenameOfSettings()}` );
+  }
   Object.entries(inenv).map(([k,v])=>{
     if ( typeof k === 'string' || typeof k === 'number' ) {
       outenv[k] = v;
@@ -12,3 +18,9 @@ const env = (settings)=>{
 };
 
 module.exports = env;
+
+const config = ()=>{
+  env( readSettings() );
+};
+
+module.exports.config = config;
