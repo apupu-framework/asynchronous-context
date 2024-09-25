ba4b2c80cb7b0242157f86bfe305d94d6c0da7b4
Atsushi Oka <oka.ats@gmail.com>
2024-09-25 19:45:09 +0900

    add `delegatorFacotry` to fix incorrectly estimated filenames of test-utils

--- test-utils.mjs
+++ test-utils.mjs
@@ -11,22 +11,84 @@ export function createTest( originalTest, createContext, options ) {
     const msg =  args.length < 2 ? 'default test' : args[first];
     const fn  = args[last];
 
-    args[last] = async (...args2 )=>{
-      try {
-        return await (
-          createContext()
+    /*
+     * === About `delegatorFactory`  ===
+     *
+     * (Wed, * 25 Sep 2024 18:45:00 +0900)
+     *
+     * ### The Problem ###
+     *
+     * The filename of the file which causes an error does not show up in the stacktrace:
+     *
+     * ```
+     *   test at ../../asynchronous-context/test-utils.mjs:25:5
+     *   read_timeline_content_confirmDeleted (6.183174ms)
+     *     TypeError [Error]: Cannot read properties of undefined (reading 'message_id')
+     *         at TKernelContext.<anonymous> (file:///home/ats/libtbc/coretbc/test/basic-kernel.test.mjs:863:37)
+     *         at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+     *         at async TKernelContext.<anonymous> (file:///home/ats/libtbc/runtime-typesafety/index.mjs:348:26)
+     *         at async TKernelContext.executeTransaction (file:///home/ats/libtbc/asynchronous-context/context.mjs:269:22)
+     *         at async args.<computed> (file:///home/ats/libtbc/asynchronous-context/test-utils.mjs:16:16)
+     *         at async Test.run (node:internal/test_runner/test:825:9)
+     *         at async Suite.processPendingSubtests (node:internal/test_runner/test:533:7)
+     *
+     *   ats@schizopod:~/libtbc/coretbc/test$ node --test --test-force-exit basic-kernel.test.mjs
+     * ```
+     *
+     * ### The Solution `delegatorFactory` ###
+     *
+     * `delegatorFactory` property helps tests to show the current stacktrace
+     * of the current function not this testex function itself.
+     *
+     * `delegatorFactory` property should always be a closure object to create
+     * a delegator function as following.
+     *
+     * ```js
+     *    delegatorFactory : fn=>function(...args){return fn.apply(this,args)} }
+     * ```
+     *
+     * `delegatorFactory` is a closure function. It is supposed to create a
+     * delegator function. For further information about delegation, see
+     * [this](https://en.wikipedia.org/wiki/Delegation_pattern).
+     *
+     * ### Result ###
+     *
+     * ```
+     *  test at basic-kernel.test.mjs:10:265
+     *  read_timeline_content_confirmDeleted (6.698337ms)
+     *    TypeError [Error]: Cannot read properties of undefined (reading 'message_id')
+     *        at TKernelContext.<anonymous> (file:///home/ats/libtbc/coretbc/test/basic-kernel.test.mjs:863:37)
+     *        at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+     *        at async TKernelContext.<anonymous> (file:///home/ats/libtbc/runtime-typesafety/index.mjs:348:26)
+     *        at async TKernelContext.executeTransaction (file:///home/ats/libtbc/asynchronous-context/context.mjs:269:22)
+     *        at async TestContext.<anonymous> (file:///home/ats/libtbc/asynchronous-context/test-utils.mjs:39:18)
+     *        at async Test.run (node:internal/test_runner/test:825:9)
+     *        at async Suite.processPendingSubtests (node:internal/test_runner/test:533:7)
+     * ```
+     *
+     */
+    const {
+      delegatorFactory = fn=>function(...args){return fn.apply(this,args)},
+    } = options;
+
+
+    args[last] = delegatorFactory(
+      async (...args2 )=>{
+        try {
+          return await (
+            createContext()
             .setOptions({ ...options, title: msg })
             .executeTransaction( AsyncContext.executeSafely( fn ),  ...args2 ));
-      } catch (e) {
-        // console.error(e);
-        throw e;
+        } catch (e) {
+          // console.error(e);
+          throw e;
+        }
       }
-    };
-    originalTest ( ...args );
+    );
+    delegatorFactory( originalTest ) ( ...args );
   }
   return testex;
 }
 
 // module.exports.createTest = createTest;
-
 
